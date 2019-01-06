c4b03a5e8ba2d34d530e26e876b1d8ae5bb64197
Krivohsey <krivoshey69@gmail.com>
2019-01-06 10:21:53 +0200

    Initial commit

---
+++ core/src/com/free/racegame/model/Car.java
@@ -0,0 +1,11 @@
+package com.free.racegame.model;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class Car extends GameObject {
+
+    public Car(Texture texture, float x, float y, float width, float height) {
+        super(texture, x, y, width, height);
+    }
+
+}
