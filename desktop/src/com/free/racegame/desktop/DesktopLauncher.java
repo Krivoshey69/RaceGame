165c1643ef1a22d5116e7bc48a0fccd96a248bf5
Krivohsey <krivoshey69@gmail.com>
2019-01-08 13:42:13 +0200

    FPS

--- desktop/src/com/free/racegame/desktop/DesktopLauncher.java
+++ desktop/src/com/free/racegame/desktop/DesktopLauncher.java
@@ -9,8 +9,6 @@ public class DesktopLauncher {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 854;
 		config.height = 480;
-		config.foregroundFPS = 0;
-		config.vSyncEnabled = false;
 		new LwjglApplication(new Main(), config);
 	}
 }
