049476d4610c9bebeba39e770291f32caf251af6
Krivohsey <krivoshey69@gmail.com>
2019-01-08 13:29:02 +0200

    add CarController in Game

--- core/src/com/free/racegame/model/Car.java
+++ core/src/com/free/racegame/model/Car.java
@@ -1,11 +1,20 @@
 package com.free.racegame.model;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.free.racegame.control.CarController;
 
 public class Car extends GameObject {
 
+    private CarController carController;
     public Car(Texture texture, float x, float y, float width, float height) {
         super(texture, x, y, width, height);
+        carController = new CarController(bounds);
     }
 
+    @Override
+    public void draw(SpriteBatch batch) {
+        super.draw(batch);
+        carController.handle();
+    }
 }
--- core/src/com/free/racegame/view/GameScreen.java
+++ core/src/com/free/racegame/view/GameScreen.java
@@ -3,6 +3,7 @@ package com.free.racegame.view;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.free.racegame.model.Car;
@@ -12,12 +13,15 @@ public class GameScreen implements Screen {
     private Texture carTexture;
     private SpriteBatch batch;
     private Car car;
+    private OrthographicCamera camera;
+    public static float deltaCff;
 
     @Override
     public void show() {
         batch = new SpriteBatch();
         carTexture = new Texture(Gdx.files.internal("car.png"));
-        car = new Car(carTexture, 0, 0, 152, 297);
+        carTexture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        car = new Car(carTexture, 0, 0, 1f, 1f * 1.97f);
     }
 
     @Override
@@ -25,6 +29,9 @@ public class GameScreen implements Screen {
         Gdx.gl.glClearColor(0,0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        deltaCff = delta;
+
+        batch.setProjectionMatrix(camera.combined);
         batch.begin();
         car.draw(batch);
         batch.end();
@@ -32,7 +39,8 @@ public class GameScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-
+        float aspectRatio = (float) height / width;
+        camera = new OrthographicCamera(20f, 20f * aspectRatio);
     }
 
     @Override
